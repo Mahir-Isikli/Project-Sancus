0d13e5d58be37bd3b552d55e2ce83791392d3e91
Mahir Isikli <isikli.mahir@gmail.com>
2022-12-30 18:33:48 +0100

    updated scraper with function

--- Twitter Scraper/scraper.py
+++ Twitter Scraper/scraper.py
@@ -2,19 +2,19 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import re as re
 
-# Created a list to append all tweet attributes(data)
-attributes_container = []
+def scrape_handle(username):
+    # Created a list to append all tweet attributes(data)
+    attributes_container = []
 
-# Using TwitterSearchScraper to scrape data and append tweets to list
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:elonmusk').get_items()):
-    if i>1000:
-        break
-    text = re.sub("@[^\s]+","",tweet.rawContent).replace("\n","").replace("  "," ").replace("  "," ")
-    if len(text) < 25: continue
-    attributes_container.append(text)
-    
-# Creating a dataframe from the tweets list above 
-tweets_df = pd.DataFrame(attributes_container, columns=["Tweets"])
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:'+username).get_items()):
+        if i>100:
+            break
+        text = re.sub("@[^\s]+","",tweet.rawContent).replace("\n","").replace("  "," ").replace("  "," ")
+        if len(text) < 25: continue
+        attributes_container.append(text)
+        
+    # Creating a dataframe from the tweets list above 
+    return pd.DataFrame(attributes_container, columns=["Tweets"])
 
-
-tweets_df.to_csv('tweets_elon.csv', encoding='utf-8',index=False)
+print(scrape_handle("elonmusk"))
